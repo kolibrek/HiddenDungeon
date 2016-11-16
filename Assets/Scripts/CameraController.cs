05c85d2ff8cb196d2ec0cf4b999f5a5d52bad2e2
Kevin Clemons <kolibermedia@gmail.com>
2016-11-15 19:46:44 -0700

    Using Standard Asset controllers, Added wall models

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -3,6 +3,9 @@ using System.Collections;
 
 public class CameraController : MonoBehaviour {
 
+    public GameObject FPSController;
+    public GameObject TPSController;
+
     public Camera TPSCamera;
     public Camera FPSCamera;
 
@@ -11,16 +14,29 @@ public class CameraController : MonoBehaviour {
     // Use this for initialization
     void Start () {
         currentCamera = 0;
-        TPSCamera.enabled = true;
-        FPSCamera.enabled = false;
+        FPSController.SetActive(false);
+        TPSController.SetActive(true);
+        //Debug.Log("FPS:" + FPSController.activeInHierarchy + " TPS: " + TPSController.activeInHierarchy);
+        //TPSCamera.enabled = true;
+        //FPSCamera.enabled = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        TPSCamera.transform.position = new Vector3(this.transform.position.x, 10f, this.transform.position.z - 10f);
+        if (TPSController.activeInHierarchy) {
+            this.transform.position = TPSController.transform.position;
+            this.transform.rotation = TPSController.transform.rotation;
+        }
+        if (FPSController.activeInHierarchy) {
+            this.transform.position = FPSController.transform.position;
+            this.transform.rotation = FPSController.transform.rotation;
+        }
+        TPSCamera.transform.position = new Vector3(this.transform.position.x, this.transform.position.y + 4f, this.transform.position.z - 2f);
 
         if (Input.GetKeyDown(KeyCode.V)) {
-            ToggleCamera();
+            //Debug.Log("Switching Controllers!");
+            Debug.Log("FPS:" + FPSController.activeInHierarchy + " TPS: " + TPSController.activeInHierarchy);
+            ToggleController();
         }
 	}
 
@@ -28,17 +44,23 @@ public class CameraController : MonoBehaviour {
         return currentCamera;
     }
 
-    void ToggleCamera() {
+    void ToggleController() {
         switch(currentCamera) {
             case 0:
                 currentCamera = 1;
-                FPSCamera.enabled = true;
                 TPSCamera.enabled = false;
+                TPSController.SetActive(false);
+                FPSController.SetActive(true);
+                FPSController.transform.position = transform.position + Vector3.up;
+                FPSController.transform.rotation = transform.rotation;
                 return;
             case 1:
                 currentCamera = 0;
                 TPSCamera.enabled = true;
-                FPSCamera.enabled = false;
+                FPSController.SetActive(false);
+                TPSController.SetActive(true);
+                TPSController.transform.position = transform.position + Vector3.down;
+                TPSController.transform.rotation = transform.rotation;
                 return;
         }
     }
